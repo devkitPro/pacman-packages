666272f54c3dd1b8582f0a0489463c68c76129b2
Dave Murphy <davem@devkitpro.org>
2018-03-19 11:42:47 +0000

    update for msys path translation

--- pkgbuild-scripts/ppcvars.sh
+++ pkgbuild-scripts/ppcvars.sh
@@ -1,7 +1,7 @@
 export DEVKITPRO=/opt/devkitpro
 export PORTLIBS_PREFIX=${DEVKITPRO}/portlibs/ppc
 
-export CPPFLAGS="-I${PORTLIBS_PREFIX}/include"
+export CPPFLAGS="-I ${PORTLIBS_PREFIX}/include"
 export LDFLAGS="-L${PORTLIBS_PREFIX}/lib"
 
 export PATH=/opt/devkitpro/portlibs/ppc/bin:$PATH
