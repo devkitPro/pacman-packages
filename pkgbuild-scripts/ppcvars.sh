f3a67cbe798fe60b89862ac357a79bcc2f485363
Dave Murphy <davem@devkitpro.org>
2018-06-25 14:43:28 +0100

    add TOOL_PREFIX

--- pkgbuild-scripts/ppcvars.sh
+++ pkgbuild-scripts/ppcvars.sh
@@ -1,6 +1,8 @@
 export DEVKITPRO=/opt/devkitpro
 export PORTLIBS_PREFIX=${DEVKITPRO}/portlibs/ppc
 
+export TOOL_PREFIX=powerpc-eabi-
+
 export CFLAGS="-O2 -mcpu=750 -meabi -mhard-float"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-DGEKKO -I ${PORTLIBS_PREFIX}/include"
