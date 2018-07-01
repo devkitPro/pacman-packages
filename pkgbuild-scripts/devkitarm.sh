4f29d524ee8893b4fa24883d0f616df62d1e4c78
Dave Murphy <davem@devkitpro.org>
2018-07-01 16:53:38 +0100

    add tools path to helper scripts

--- pkgbuild-scripts/devkitarm.sh
+++ pkgbuild-scripts/devkitarm.sh
@@ -2,7 +2,7 @@
 export DEVKITPRO=/opt/devkitpro
 export DEVKITARM=${DEVKITPRO}/devkitARM
 export PORTLIBS_ROOT=${DEVKITPRO}/portlibs
-export PATH=$DEVKITARM/bin:$PATH
+export PATH=${DEVKITPRO}/tools/bin:$DEVKITARM/bin:$PATH
 export TOOL_PREFIX=arm-none-eabi-
 export AR=${TOOL_PREFIX}gcc-ar
 export RANLIB=${TOOL_PREFIX}gcc-ranlib
