7b70b4f3b5aad75b69359a03d71d9f36ac8904d5
fincs <fincs.alt1@gmail.com>
2018-07-31 21:19:05 +0200

    Update CFLAGS in pkgbuild helper scripts

--- pkgbuild-scripts/3dsvars.sh
+++ pkgbuild-scripts/3dsvars.sh
@@ -3,7 +3,7 @@
 PORTLIBS_PREFIX=${PORTLIBS_ROOT}/3ds
 PATH=$PORTLIBS_PREFIX/bin:$PATH
 
-export CFLAGS="-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft -O2 -mword-relocations -ffunction-sections"
+export CFLAGS="-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft -O2 -mword-relocations -ffunction-sections -fdata-sections"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-D_3DS -I ${PORTLIBS_PREFIX}/include -I ${DEVKITPRO}/libctru/include"
 export LDFLAGS="-L${PORTLIBS_PREFIX}/lib -L${DEVKITPRO}/libctru/lib"
--- pkgbuild-scripts/switchvars.sh
+++ pkgbuild-scripts/switchvars.sh
@@ -3,7 +3,7 @@
 export PORTLIBS_PREFIX=${DEVKITPRO}/portlibs/switch
 export PATH=$PORTLIBS_PREFIX/bin:$PATH
 
-export CFLAGS="-O2 -march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec"
+export CFLAGS="-O2 -ffunction-sections -fdata-sections -march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-D__SWITCH__ -I ${PORTLIBS_PREFIX}/include -isystem ${DEVKITPRO}/libnx/include"
 export LDFLAGS="-O2 -march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec -L${PORTLIBS_PREFIX}/lib -L${DEVKITPRO}/libnx/lib"
