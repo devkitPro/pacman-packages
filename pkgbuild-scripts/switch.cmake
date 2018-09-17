4d0201c5657bb230ef47a62b666a365c6f1ac9d5
Dave Murphy <davem@devkitpro.org>
2018-09-17 15:51:24 +0100

    remove duplicate lines

--- pkgbuild-scripts/switch.cmake
+++ pkgbuild-scripts/switch.cmake
@@ -6,11 +6,8 @@ set(SWITCH_LIBNX TRUE)
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIE -ftls-model=local-exec -L/opt/devkitpro/portlibs/switch/lib -L/opt/devkitpro/libnx/lib -specs=/opt/devkitpro/libnx/switch.specs")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT}")
 
-set(SWITCH_LIBNX TRUE)
 set(CMAKE_FIND_ROOT_PATH /opt/devkitpro/portlibs/switch/)
 set(CMAKE_PREFIX_PATH "/opt/devkitpro/portlibs/switch/")
-
-SET(CMAKE_FIND_ROOT_PATH /opt/devkitpro/portlibs/switch)
 
 include(devkita64.cmake)
 
