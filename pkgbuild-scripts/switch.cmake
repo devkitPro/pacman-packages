39ff88d9af96cc0801f7bc73ae57e1c6b6311a49
Jules Blok <jules.blok@gmail.com>
2018-12-22 14:25:36 +0100

    Add compiler flags to switch.cmake

--- pkgbuild-scripts/switch.cmake
+++ pkgbuild-scripts/switch.cmake
@@ -3,6 +3,9 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(SWITCH_LIBNX TRUE)
 
+set(CMAKE_C_FLAGS "-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIE")
+set(CMAKE_CXX_FLAGS "-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIE")
+
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIE -ftls-model=local-exec -L/opt/devkitpro/portlibs/switch/lib -L/opt/devkitpro/libnx/lib -specs=/opt/devkitpro/libnx/switch.specs")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT}")
 
