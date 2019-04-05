ab0eaadca67dde76a02aa7231463feb324d0a601
fincs <fincs@devkitpro.org>
2019-04-05 14:29:37 +0200

    Update switch arch setting

--- pkgbuild-scripts/switch.cmake
+++ pkgbuild-scripts/switch.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(SWITCH_LIBNX TRUE)
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-march=armv8-a -mtune=cortex-a57 -mtp=soft -fPIE -ftls-model=local-exec -L/opt/devkitpro/portlibs/switch/lib -L/opt/devkitpro/libnx/lib -specs=/opt/devkitpro/libnx/switch.specs")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -fPIE -ftls-model=local-exec -L/opt/devkitpro/portlibs/switch/lib -L/opt/devkitpro/libnx/lib -specs=/opt/devkitpro/libnx/switch.specs")
 set(CMAKE_MODULE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT}")
 
 set(CMAKE_FIND_ROOT_PATH /opt/devkitpro/portlibs/switch/)
