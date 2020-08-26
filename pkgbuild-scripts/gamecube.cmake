ec7c68c269aee5f88d5d4b65bb6d7d12bfd1b63d
Dave Murphy <davem@devkitpro.org>
2020-08-26 03:34:25 +0100

    pkgbuild-scripts: prefer CMAKE_EXE_LINKER_FLAGS_INIT

--- pkgbuild-scripts/gamecube.cmake
+++ pkgbuild-scripts/gamecube.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR powerpc)
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -mogc")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-mogc")
 
 set(CMAKE_FIND_ROOT_PATH /opt/devkitpro/portlibs/gamecube /opt/devkitpro/portlibs/ppc)
 
--- pkgbuild-scripts/wii.cmake
+++ pkgbuild-scripts/wii.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR powerpc)
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -mrvl")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-mrvl")
 
 set(CMAKE_FIND_ROOT_PATH /opt/devkitpro/portlibs/wii /opt/devkitpro/portlibs/ppc)
 
