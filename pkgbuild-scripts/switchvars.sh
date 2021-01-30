2e9c76f1a3e4afd3788856922cf2bb18afb45093
Dave Murphy <davem@devkitpro.org>
2021-01-30 14:46:18 +0000

    pkgbuild-scripts: allow msys2 path translation

--- pkgbuild-scripts/switchvars.sh
+++ pkgbuild-scripts/switchvars.sh
@@ -6,6 +6,6 @@ export PATH=$PORTLIBS_PREFIX/bin:$PATH
 export ARCH="-march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -fPIC -ftls-model=local-exec"
 export CFLAGS="${ARCH} -O2 -ffunction-sections -fdata-sections"
 export CXXFLAGS="${CFLAGS}"
-export CPPFLAGS="-D__SWITCH__ -I${PORTLIBS_PREFIX}/include -isystem${DEVKITPRO}/libnx/include"
+export CPPFLAGS="-D__SWITCH__ -I ${PORTLIBS_PREFIX}/include -isystem ${DEVKITPRO}/libnx/include"
 export LDFLAGS="${ARCH} -L${PORTLIBS_PREFIX}/lib -L${DEVKITPRO}/libnx/lib"
 export LIBS="-lnx"
