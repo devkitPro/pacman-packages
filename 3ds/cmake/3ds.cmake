b7166f2e4df0e8334ec515cf5ad7a91869182351
fincs <fincs@devkitpro.org>
2021-05-16 01:44:27 +0200

    3ds-cmake: bring back link directory sanity

--- 3ds/cmake/3ds.cmake
+++ 3ds/cmake/3ds.cmake
@@ -2,23 +2,26 @@ include(/opt/devkitpro/devkitARM/share/devkitARM.cmake)
 
 set(CMAKE_SYSTEM_PROCESSOR "armv6k")
 
-set (DKA_3DS_C_FLAGS "-D__3DS__ -march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft -mword-relocations -ffunction-sections")
-set(CMAKE_C_FLAGS   "${DKA_3DS_C_FLAGS}" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "${DKA_3DS_C_FLAGS}" CACHE STRING "")
-set(CMAKE_ASM_FLAGS "${DKA_3DS_C_FLAGS}" CACHE STRING "")
+set(CTR_ARCH_SETTINGS "-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft")
+set(CTR_COMMON_FLAGS  "${CTR_ARCH_SETTINGS} -mword-relocations -ffunction-sections -D__3DS__")
+set(CTR_LIB_DIRS      "-L${DEVKITPRO}/libctru/lib -L${DEVKITPRO}/portlibs/3ds/lib")
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-march=armv6k -mtune=mpcore -mfloat-abi=hard -mtp=soft -specs=3dsx.specs")
+set(CMAKE_C_FLAGS_INIT   "${CTR_COMMON_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${CTR_COMMON_FLAGS}")
+set(CMAKE_ASM_FLAGS_INIT "${CTR_COMMON_FLAGS}")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${CTR_ARCH_SETTINGS} ${CTR_LIB_DIRS} -specs=3dsx.specs")
 
 set(CMAKE_FIND_ROOT_PATH
-  ${CMAKE_FIND_ROOT_PATH}
-  ${DEVKITPRO}/portlibs/3ds
-  ${DEVKITPRO}/libctru
+	${CMAKE_FIND_ROOT_PATH}
+	${DEVKITPRO}/portlibs/3ds
+	${DEVKITPRO}/libctru
 )
 
 # Set pkg-config for the same
 find_program(PKG_CONFIG_EXECUTABLE NAMES arm-none-eabi-pkg-config HINTS "${DEVKITPRO}/portlibs/3ds/bin")
 if (NOT PKG_CONFIG_EXECUTABLE)
-   message(WARNING "Could not find arm-none-eabi-pkg-config: try installing 3ds-pkg-config")
+	message(WARNING "Could not find arm-none-eabi-pkg-config: try installing 3ds-pkg-config")
 endif()
 
 find_program(CTR_SMDHTOOL_EXE NAMES smdhtool HINTS "${DEVKITPRO}/tools/bin")
@@ -95,7 +98,7 @@ set(NINTENDO_3DS TRUE)
 
 set(CTR_ROOT ${DEVKITPRO}/libctru)
 
-set(CTR_STANDARD_LIBRARIES "${CTR_ROOT}/lib/libctru.a -lm")
+set(CTR_STANDARD_LIBRARIES "-lctru -lm")
 set(CMAKE_C_STANDARD_LIBRARIES "${CTR_STANDARD_LIBRARIES}" CACHE STRING "" FORCE)
 set(CMAKE_CXX_STANDARD_LIBRARIES "${CTR_STANDARD_LIBRARIES}" CACHE STRING "" FORCE)
 set(CMAKE_ASM_STANDARD_LIBRARIES "${CTR_STANDARD_LIBRARIES}" CACHE STRING "" FORCE)
@@ -105,5 +108,3 @@ set(CTR_STANDARD_INCLUDE_DIRECTORIES "${CTR_ROOT}/include")
 set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES "${CTR_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
 set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES "${CTR_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
 set(CMAKE_ASM_STANDARD_INCLUDE_DIRECTORIES "${CTR_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
-
-link_directories( ${DEVKITPRO}/libctru/lib ${DEVKITPRO}/portlibs/3ds/lib )
