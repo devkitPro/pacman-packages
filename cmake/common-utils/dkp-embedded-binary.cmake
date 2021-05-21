c3b61a3efe6b839dc31880cd25242b82524241b8
fincs <fincs@devkitpro.org>
2021-05-21 23:38:43 +0200

    cmake: improve embedded binary support

--- cmake/common-utils/dkp-embedded-binary.cmake
+++ cmake/common-utils/dkp-embedded-binary.cmake
@@ -5,9 +5,9 @@ if (NOT BIN2S_EXE)
 	message(WARNING "Could not find bin2s: try installing general-tools")
 endif()
 
-function(dkp_embed_binaries outvar)
+function(dkp_generate_binary_embed_sources outvar)
 	if (NOT ${ARGC} GREATER 1)
-		message(FATAL_ERROR "dkp_generate_binary: must provide at least one input file")
+		message(FATAL_ERROR "dkp_generate_binary_embed_sources: must provide at least one input file")
 	endif()
 
 	set(outlist "")
@@ -20,7 +20,7 @@ function(dkp_embed_binaries outvar)
 			COMMAND ${BIN2S_EXE} -H "${CMAKE_CURRENT_BINARY_DIR}/${basename}.h" "${infile}" > "${CMAKE_CURRENT_BINARY_DIR}/${basename}.s"
 			DEPENDS "${infile}"
 			WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-			COMMENT "Generating embedded binary for ${infile}"
+			COMMENT "Generating binary embedding source for ${infile}"
 		)
 
 		list(APPEND outlist "${CMAKE_CURRENT_BINARY_DIR}/${basename}.s" "${CMAKE_CURRENT_BINARY_DIR}/${basename}.h")
@@ -29,7 +29,19 @@ function(dkp_embed_binaries outvar)
 	set(${outvar} "${outlist}" PARENT_SCOPE)
 endfunction()
 
-function(dkp_target_embedded_binaries target)
-	dkp_embed_binaries(intermediates ${ARGN})
-	target_sources(${target} PRIVATE ${intermediates})
+function(dkp_add_embedded_binary_library target)
+	dkp_generate_binary_embed_sources(intermediates ${ARGN})
+	add_library(${target} OBJECT ${intermediates})
+	target_include_directories(${target} INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
+
+function(dkp_target_use_embedded_binary_libraries target)
+	if (NOT ${ARGC} GREATER 1)
+		message(FATAL_ERROR "dkp_target_use_embedded_binary_libraries: must provide at least one input library")
+	endif()
+
+	foreach (libname ${ARGN})
+		target_sources(${target} PRIVATE $<TARGET_OBJECTS:${libname}>)
+		target_include_directories(${target} PRIVATE $<TARGET_PROPERTY:${libname},INTERFACE_INCLUDE_DIRECTORIES>)
+	endforeach()
 endfunction()
