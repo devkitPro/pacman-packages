7da57ca19aca4b333f82617deeec5a9600e580ee
fincs <fincs@devkitpro.org>
2022-07-25 21:26:30 +0000

    wiiuvars: Add complete compiler and linker flags

--- dkp-toolchain-vars/wiiuvars.sh
+++ dkp-toolchain-vars/wiiuvars.sh
@@ -4,7 +4,10 @@ export PORTLIBS_PREFIX=${PORTLIBS_ROOT}/wiiu
 export PORTLIBS_PPC=${PORTLIBS_ROOT}/ppc
 export PORTLIBS_WIIU=${PORTLIBS_PREFIX}
 
-export CPPFLAGS="-D__WIIU__ -I${PORTLIBS_WIIU}/include -I${PORTLIBS_PPC}/include"
-export LDFLAGS="-L${PORTLIBS_WIIU}/lib -L${PORTLIBS_PPC}/lib"
+export CFLAGS="-mcpu=750 -meabi -mhard-float -O2 -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
+export CPPFLAGS="-DESPRESSO -D__WIIU__ -D__WUT__ -I${PORTLIBS_WIIU}/include -I${PORTLIBS_PPC}/include -I${DEVKITPRO}/wut/include"
+export LDFLAGS="-L${PORTLIBS_WIIU}/lib -L${PORTLIBS_PPC}/lib -L${DEVKITPRO}/wut/lib -specs=${DEVKITPRO}/wut/share/wut.specs"
+export LIBS="-lwut -lm"
 
 export PATH=${PORTLIBS_WIIU}/bin:${PORTLIBS_PPC}/bin:$PATH
