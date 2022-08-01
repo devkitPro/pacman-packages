60eedb67b64570f29271f814a7bf553e6900a572
Harrison <53527582+HTV04@users.noreply.github.com>
2022-07-31 20:57:42 -0400

    dkp-toolchain-vars: Add missing CFLAGS and CXXFLAGS
    `cubevars.sh` and `wiivars.sh` are missing `CFLAGS` and `CXXFLAGS`.

--- dkp-toolchain-vars/cubevars.sh
+++ dkp-toolchain-vars/cubevars.sh
@@ -4,6 +4,8 @@ export PORTLIBS_PREFIX=${PORTLIBS_ROOT}/gamecube
 export PORTLIBS_PPC=${PORTLIBS_ROOT}/ppc
 export PORTLIBS_CUBE=${PORTLIBS_PREFIX}
 
+export CFLAGS="-O2 -mogc -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-D__GAMECUBE__ -I${PORTLIBS_CUBE}/include -I${PORTLIBS_PPC}/include"
 export LDFLAGS="-L${PORTLIBS_CUBE}/lib -L${PORTLIBS_PPC}/lib"
 
