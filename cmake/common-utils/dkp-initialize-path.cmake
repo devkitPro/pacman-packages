423ea3b270baa7e6005161d1ee31202b9a94f028
sam1677 <leejihyek1267@gmail.com>
2022-08-10 17:26:50 +0900

    dkp-cmake-common-utils: use TO_CMAKE_PATH to escape windows path

--- cmake/common-utils/dkp-initialize-path.cmake
+++ cmake/common-utils/dkp-initialize-path.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.13)
 if(NOT DEFINED ENV{DEVKITPRO})
 	set(DEVKITPRO /opt/devkitpro)
 else()
-	set(DEVKITPRO $ENV{DEVKITPRO})
+	file(TO_CMAKE_PATH $ENV{DEVKITPRO} DEVKITPRO)
 endif()
 
 list(APPEND CMAKE_MODULE_PATH "${DEVKITPRO}/cmake")
