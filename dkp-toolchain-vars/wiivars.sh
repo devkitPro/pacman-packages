3f4ed4ea96610d4701e116cdbf4e80b2ed088013
Harrison <53527582+HTV04@users.noreply.github.com>
2022-09-09 07:53:48 -0400

    dkp-toolchain-vars: Add missing CFLAGS and CXXFLAGS (#277)

--- dkp-toolchain-vars/wiivars.sh
+++ dkp-toolchain-vars/wiivars.sh
@@ -4,6 +4,8 @@ export PORTLIBS_PREFIX=${PORTLIBS_ROOT}/wii
 export PORTLIBS_PPC=${PORTLIBS_ROOT}/ppc
 export PORTLIBS_WII=${PORTLIBS_PREFIX}
 
+export CFLAGS="-O2 -mrvl -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
+export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-D__WII__ -I${PORTLIBS_WII}/include -I${PORTLIBS_PPC}/include"
 export LDFLAGS="-L${PORTLIBS_WII}/lib -L${PORTLIBS_PPC}/lib"
 
