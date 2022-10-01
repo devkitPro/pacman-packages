67ac5022f668a2c1f02e1571f93f8a38bc2ff91e
Nosh (97) Ware <30322411+Nosh-Ware@users.noreply.github.com>
2022-10-01 13:05:23 -0500

    Made NINTENDO_GAMECUBE cached

--- cmake/gamecube/NintendoGameCube.cmake
+++ cmake/gamecube/NintendoGameCube.cmake
@@ -5,7 +5,7 @@ cmake_minimum_required(VERSION 3.13)
 include_guard(GLOBAL)
 
 # Platform identification flags
-set(NINTENDO_GAMECUBE TRUE)
+set(NINTENDO_GAMECUBE CACHE BOOL TRUE)
 
 # Inherit libogc platform configuration
 include(Platform/libogc)
