081f039bd6d8b1e24d629ce7caf2033afc3de410
Nosh (97) Ware <30322411+Nosh-Ware@users.noreply.github.com>
2022-10-01 13:08:14 -0500

    Made NINTENDO_WII cached

--- cmake/wii/NintendoWii.cmake
+++ cmake/wii/NintendoWii.cmake
@@ -5,7 +5,7 @@ cmake_minimum_required(VERSION 3.13)
 include_guard(GLOBAL)
 
 # Platform identification flags
-set(NINTENDO_WII TRUE)
+set(NINTENDO_WII CACHE BOOL TRUE)
 set(OGC_EXTRA_LIBS "-lwiiuse -lbte")
 
 # Inherit libogc platform configuration
