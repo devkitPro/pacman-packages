6907fe2e026bf4c4bbcec7810ff40a69b1b13020
Dave Murphy <WinterMute@users.noreply.github.com>
2023-04-22 17:39:06 +0100

    set alignment in dkp_add_embedded_binary_library for each toolchain (#297)

--- cmake/devkita64/devkitA64.cmake
+++ cmake/devkita64/devkitA64.cmake
@@ -1,4 +1,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/dkp-initialize-path.cmake)
 include(dkp-toolchain-common)
 
+set (DKP_BIN2S_ALIGNMENT 8)
+
 __dkp_toolchain(devkitA64 aarch64 aarch64-none-elf)
--- cmake/devkitarm/devkitARM.cmake
+++ cmake/devkitarm/devkitARM.cmake
@@ -1,4 +1,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/dkp-initialize-path.cmake)
 include(dkp-toolchain-common)
 
+set (DKP_BIN2S_ALIGNMENT 4)
+
 __dkp_toolchain(devkitARM arm arm-none-eabi)
