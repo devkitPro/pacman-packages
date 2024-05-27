867f2e67f001cb009b65c5b130c490398386c0be
Alberto Mardegan <mardy@users.sourceforge.net>
2024-05-27 22:09:12 +0300

    dkp-toolchain-vars: add more variables to GameCube & Wii scripts (#352)
    Add the libogc include directory to CPPFLAGS.

--- dkp-toolchain-vars/cubevars.sh
+++ dkp-toolchain-vars/cubevars.sh
@@ -6,7 +6,7 @@ export PORTLIBS_CUBE=${PORTLIBS_PREFIX}
 
 export CFLAGS="-O2 -mogc -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
 export CXXFLAGS="${CFLAGS}"
-export CPPFLAGS="-D__GAMECUBE__ -I${PORTLIBS_CUBE}/include -I${PORTLIBS_PPC}/include"
+export CPPFLAGS="-D__GAMECUBE__ -I${DEVKITPRO}/libogc/include -I${PORTLIBS_CUBE}/include -I${PORTLIBS_PPC}/include"
 export LDFLAGS="-L${PORTLIBS_CUBE}/lib -L${PORTLIBS_PPC}/lib"
 
 export PATH=${PORTLIBS_CUBE}/bin:${PORTLIBS_PPC}/bin:$PATH
--- dkp-toolchain-vars/wiivars.sh
+++ dkp-toolchain-vars/wiivars.sh
@@ -6,7 +6,7 @@ export PORTLIBS_WII=${PORTLIBS_PREFIX}
 
 export CFLAGS="-O2 -mrvl -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
 export CXXFLAGS="${CFLAGS}"
-export CPPFLAGS="-D__WII__ -I${PORTLIBS_WII}/include -I${PORTLIBS_PPC}/include"
+export CPPFLAGS="-D__WII__ -I${DEVKITPRO}/libogc/include -I${PORTLIBS_WII}/include -I${PORTLIBS_PPC}/include"
 export LDFLAGS="-L${PORTLIBS_WII}/lib -L${PORTLIBS_PPC}/lib"
 
 export PATH=${PORTLIBS_WII}/bin:${PORTLIBS_PPC}/bin:$PATH
