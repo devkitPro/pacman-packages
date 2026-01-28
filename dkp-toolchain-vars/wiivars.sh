3ffe9b529b2a4a08868c641aa5a1a839590e0f3b
Dave Murphy <davem@devkitpro.org>
2026-01-28 17:11:05 +0000

    wiivars: add ogc to libs

--- dkp-toolchain-vars/wiivars.sh
+++ dkp-toolchain-vars/wiivars.sh
@@ -7,6 +7,7 @@ export PORTLIBS_WII=${PORTLIBS_PREFIX}
 export CFLAGS="-O2 -mrvl -mcpu=750 -meabi -mhard-float -ffunction-sections -fdata-sections"
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-D__WII__ -I${DEVKITPRO}/libogc/include -I${PORTLIBS_WII}/include -I${PORTLIBS_PPC}/include"
-export LDFLAGS="-L${PORTLIBS_WII}/lib -L${PORTLIBS_PPC}/lib"
+export LDFLAGS="-L${PORTLIBS_WII}/lib -L${PORTLIBS_PPC}/lib -L${DEVKITPRO}/libogc/lib/wii"
+export LIBS="-logc -lm"
 
 export PATH=${PORTLIBS_WII}/bin:${PORTLIBS_PPC}/bin:$PATH
